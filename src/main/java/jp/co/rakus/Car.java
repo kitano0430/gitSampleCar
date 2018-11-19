9d566b91639d53280b90af1dc7a83e12698e0ba4
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-19 11:45:16 +0900

    フィールド変数にname, getter/setter追加

--- src/main/java/jp/co/rakus/Car.java
+++ src/main/java/jp/co/rakus/Car.java
@@ -4,5 +4,26 @@ public class Car {
 	
 	private int speed;
 	private String color;
+	private String name;
+	
+	//getter,setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 }
