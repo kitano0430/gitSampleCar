36cea8d655508ca262e3606ed4daf7afdb36d297
ohyamajunpei <junpei.oyama@rakus.co.jp>
2018-11-19 12:24:13 +0900

    競合を修正

--- src/main/java/jp/co/rakus/Car.java
+++ src/main/java/jp/co/rakus/Car.java
@@ -1,24 +1,6 @@
 package jp.co.rakus;
 
 public class Car {
-<<<<<<< HEAD
-
-	public int speed;
-	public String color;
-
-	public int getSpeed() {
-		return speed;
-	}
-
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-=======
 	
 	private int speed;
 	private String color;
@@ -40,7 +22,6 @@ public class Car {
 	public String getColor() {
 		return color;
 	}
->>>>>>> feature/login
 	public void setColor(String color) {
 		this.color = color;
 	}
